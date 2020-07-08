84effdae6ed1f4d108108205fc9ff7934190774a
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-07-08 20:41:52 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,3 +1,17 @@
-class Hero < ActiveRecord::Base
+class Team
+  
+  attr_reader :name, :power, :bio
+ 
+  TEAMS = []
+ 
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    TEAMS << self
+  end
+ 
+  def self.all
+    TEAMS
+  end
   
 end
